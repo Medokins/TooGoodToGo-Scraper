73c2887ae20e1ff13f20afb48055a051e7071f5e
Medokins <nikodemszwast@gmail.com>
2022-07-03 19:15:25 +0200

    fix radius change

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from menu import Menu, runMenu
 with open('data.txt') as f:
     lines = f.readlines()
 
+# search radius
+user_radius = 1
+
 #[:-1] to not read in new-line character
 user_access_token = lines[0][:-1]
 user_refresh_token = lines[1][:-1]
@@ -18,14 +21,22 @@ client = TgtgClient(access_token = user_access_token,
                     refresh_token = user_refresh_token,
                     user_id = user_id)
 
-favourite_stores = runMenu()
+items = client.get_items(
+        favorites_only = False,
+        latitude = user_latitude,
+        longitude = user_longitude,
+        radius = user_radius
+    )
+
+shops = np.unique([item["store"]["store_name"] for item in items])
+favourite_stores = runMenu(shops)
 
 while True:
     items = client.get_items(
         favorites_only = False,
         latitude = user_latitude,
         longitude = user_longitude,
-        radius = 3
+        radius = user_radius
     )
     for item in items:
         shop = item["store"]["store_name"]
